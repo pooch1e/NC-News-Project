70f6806d7dd711fb0f9247ceeba2eca6cca2c957
Joel Kram <joelnskra@gmail.com>
2025-05-30 13:54:11 +0100

    refactored util tests for new function

--- db/queries.js
+++ db/queries.js
@@ -22,35 +22,35 @@ const db = require('./connection');
 //   });
 
 // Get all of the comments where the votes are less than zero
-db.query(`SELECT article_id, body FROM comments WHERE votes < 0`)
-  .then(({ rows }) => {
-    console.log(rows);
-  })
-  .catch((err) => {
-    return err;
-  });
+// db.query(`SELECT article_id, body FROM comments WHERE votes < 0`)
+//   .then(({ rows }) => {
+//     console.log(rows);
+//   })
+//   .catch((err) => {
+//     return err;
+//   });
 
 // Get all of the topics
-db.query('SELECT topic FROM articles')
-  .then(({ rows }) => {
-    console.log(rows);
-  })
-  .catch((err) => {
-    return err;
-  });
+// db.query('SELECT topic FROM articles')
+//   .then(({ rows }) => {
+//     console.log(rows);
+//   })
+//   .catch((err) => {
+//     return err;
+//   });
 
 // Get all of the articles by user grumpy19
-db.query(`SELECT articles.body FROM articles WHERE author = 'grumpy19'`)
-  .then(({ rows }) => {
-    console.log(rows);
-  })
-  .catch((err) => {
-    return err;
-  });
+// db.query(`SELECT articles.body FROM articles WHERE author = 'grumpy19'`)
+//   .then(({ rows }) => {
+//     console.log(rows);
+//   })
+//   .catch((err) => {
+//     return err;
+//   });
 
 // Get all of the comments that have more than 10 votes.
 
-db.query(`SELECT body FROM comments WHERE votes > 10`)
+db.query(`SELECT * FROM comments WHERE votes > 10`)
   .then(({ rows }) => {
     console.log(rows);
   })
