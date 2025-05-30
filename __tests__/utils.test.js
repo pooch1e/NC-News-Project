fa64115670244f250a605e4ebb2541bc0c46b948
Joel Kram <joelnskra@gmail.com>
2025-05-30 14:14:33 +0100

    removed comments from util tests and added back in async jest wait

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -146,101 +146,12 @@ describe('tests for getValueFromKey function', () => {
   test('does not mutate original array', () => {
     const data = [{ name: 'Rose', id: 'dS8rJns', secretFear: 'spiders' }];
     const dataCopy = data.map((obj) => {
-      return obj
-    })
+      return obj;
+    });
     getValueFromKey(data);
     expect(dataCopy).toEqual(data);
   });
 });
-
-// describe('getValueFromKey', () => {
-//   test('returns empty object when passed empty array', () => {
-//     const inputArray = [];
-//     const actual = getValueFromKey(inputArray, 'key', 'value');
-//     expect(actual).toEqual({});
-//   });
-//   test('returns an object with the correct key and value referenced when passed an array with a single object', () => {
-//     const inputArray = [
-//       {
-//         article_title: 'Living in the shadow of a great man',
-//         body: 'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
-//         votes: 14,
-//         author: 'butter_bridge',
-//         created_at: '2020-07-21T01:20:00.000Z',
-//       },
-//     ];
-
-//     const expected = [
-//       {
-//         article_title: 'Living in the shadow of a great man',
-//         body: 'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
-//         votes: 14,
-//         author: 'butter_bridge',
-//         created_at: '2020-07-21T01:20:00.000Z',
-//       },
-//     ];
-
-//     const actual = getValueFromKey(
-//       inputArray,
-//       'article_title',
-//       'Living in the shadow of a great man'
-//     );
-//     expect(actual).toEqual(expected);
-//   });
-//   test('returns an object with the correct key and value referenced when passed an array of multiple objects', () => {
-//     const inputArray = [
-//       {
-//         title: 'Living in the shadow of a great man',
-//         topic: 'mitch',
-//         author: 'butter_bridge',
-//         body: 'I find this existence challenging',
-//         created_at: 1594329060000,
-//         votes: 100,
-//         article_img_url:
-//           'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
-//       },
-//       {
-//         title: 'Sony Vaio; or, The Laptop',
-//         topic: 'mitch',
-//         author: 'icellusedkars',
-//         body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
-//         created_at: 1602828180000,
-//         votes: 0,
-//         article_img_url:
-//           'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
-//       },
-//       {
-//         title: 'Eight pug gifs that remind me of mitch',
-//         topic: 'mitch',
-//         author: 'icellusedkars',
-//         body: 'some gifs',
-//         created_at: 1604394720000,
-//         votes: 0,
-//         article_img_url:
-//           'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
-//       },
-//     ];
-//     const expected = [
-//       {
-//         title: 'Living in the shadow of a great man',
-//         topic: 'mitch',
-//         author: 'butter_bridge',
-//         body: 'I find this existence challenging',
-//         created_at: 1594329060000,
-//         votes: 100,
-//         article_img_url:
-//           'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
-//       },
-//     ];
-//     const actual = getValueFromKey(
-//       inputArray,
-//       'title',
-//       'Living in the shadow of a great man'
-//     );
-//     expect(actual).toEqual(expected);
-//   });
-//   test.todo('does not mutate input');
-// });
 
 afterAll(async () => {
   await db.end();
