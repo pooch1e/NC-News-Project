7b2332244763a28547f3397da4912c3f827c40d7
Joel Kram <joelnskra@gmail.com>
2025-05-30 14:44:05 +0100

    planned user_topics and removed unnecssary comments

--- db/data/test-data/user_topics.js
+++ db/data/test-data/user_topics.js
@@ -2,3 +2,14 @@
 // ! cols user_topic_id PK 
 // ! username FK
 // ! topic FK
+
+const user_topics = [
+  { username: 'butter_bridge', topic: 'coding' },
+  { username: 'icellusedkars', topic: 'football' },
+  { username: 'rogersop', topic: 'cooking' },
+  { username: 'lurker', topic: 'coding' },
+  { username: 'butter_bridge', topic: 'cooking' },
+  { username: 'icellusedkars', topic: 'coding' },
+  { username: 'lurker', topic: 'football' },
+  { username: 'rogersop', topic: 'coding' }
+];
