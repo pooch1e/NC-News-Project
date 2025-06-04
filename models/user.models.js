fd1785bf6b3e6f56c6ce97583c9f3f76ed47f860
Joel Kram <joelnskra@gmail.com>
2025-06-04 16:12:17 +0100

    fix: cleaned comments and updated endpoints.json

--- models/user.models.js
+++ models/user.models.js
@@ -1,10 +1,9 @@
 const db = require('../db/connection');
 
 const fetchUser = () => {
-  // console.log('hello from user model');
-  return db.query(`SELECT * FROM users`).then(({rows}) => {
+  return db.query(`SELECT * FROM users`).then(({ rows }) => {
     return rows;
-  })
+  });
 };
 
 module.exports = fetchUser;
