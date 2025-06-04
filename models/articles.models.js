0f84091f15a0f3703789f7fa1eacc07cf003b679
Joel Kram <joelnskram@gmail.com>
2025-06-04 15:27:08 +0100

    impl: fix for returning string types from db with pg types and and implemented articles endpoint

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,11 +2,19 @@
 const db = require('../db/connection');
 
 const fetchArticles = () => {
-  console.log('hello from articles models');
-  return db.query(`SELECT * FROM articles`).then(({ rows }) => {
-    console.log(rows);
-    return rows;
-  });
+  // console.log('hello from articles models');
+  return db
+    .query(
+      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id`
+    )
+    .then(({ rows }) => {
+      //join comments onto articles
+      //count comments per id
+      //map over rows
+      //add comment_count to objects
+
+      return rows;
+    });
 };
 
 module.exports = fetchArticles;
