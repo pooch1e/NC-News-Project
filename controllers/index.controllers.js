7e937776c71ed6cce097311569c24ef01ca814ba
Joel Kram <joelnskram@gmail.com>
2025-06-04 13:13:02 +0100

    added: articles routing

---
+++ controllers/index.controllers.js
@@ -0,0 +1,7 @@
+//index.controllers
+
+const getApi = require('./api.controller');
+const getTopics = require('./topics.controller');
+const getArticles = require('./articles.controller');
+
+module.exports = { getApi, getTopics, getArticles };
