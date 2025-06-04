0e1007028cf6062477ccf37d5d8133caa42c21ae
Joel Kram <joelnskra@gmail.com>
2025-06-04 10:55:25 +0100

    impl: listen.js

--- listener.js
+++ listener.js
@@ -1,1 +1,11 @@
 //listen.js
+const app = require('../northcoders-news-BE/app');
+const port = 8000;
+
+app.listen(port, (err) => {
+  if (err) {
+    return err;
+  } else {
+    return console.log(`listening on ${port}`);
+  }
+});
