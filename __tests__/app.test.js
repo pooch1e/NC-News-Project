2255f4aa9578edbc3f491438820e358d7d890076
Joel Kram <joelnskra@gmail.com>
2025-06-04 15:27:08 +0100

    impl: fix for returning string types from db with pg types and and implemented articles endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,20 @@
-const endpointsJson = require("../endpoints.json");
+const endpointsJson = require('../endpoints.json');
 
-const db = require("../db/connection");
-const seed = require("../db/seeds/seed");
-const data = require("../db/data/test-data");
-const request = require("supertest");
-const app = require("../app");
-
+const db = require('../db/connection');
+const seed = require('../db/seeds/seed');
+const data = require('../db/data/test-data');
+const request = require('supertest');
+const app = require('../app');
 
 /* Set up your test imports here */
 
 beforeEach(() => {
   return seed(data);
-})
+});
 
 afterAll(() => {
   return db.end();
-})
+});
 
 describe('GET /api', () => {
   test('200: Responds with an object detailing the documentation for each endpoint', () => {
@@ -30,25 +29,36 @@ describe('GET /api', () => {
 describe('GET api/topics', () => {
   test('200: Responds with an object detailing all topics', () => {
     return request(app)
-    .get('/api/topics')
-    .expect(200)
-    .then(({body: {topics}}) => {
-      expect(topics.length).not.toBe(0);
-      topics.forEach((topic) => {
-        expect(typeof topic.slug).toBe('string')
-        expect(typeof topic.description).toBe('string')
-      })
-    })
-  })
-})
+      .get('/api/topics')
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length).not.toBe(0);
+        topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe('string');
+          expect(typeof topic.description).toBe('string');
+        });
+      });
+  });
+});
 
-describe('GET api/articles', () => {
+describe.only('GET api/articles', () => {
   test('200: Responds with an object with the key of articles and the value of an array of article objects', () => {
     return request(app)
-    .get('/api/articles')
-    .expect(200)
-    .then(({body}) => {
-      console.log(body);
-    })
-  })
-})
+      .get('/api/articles')
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        expect(articles.length).not.toBe(0);
+
+        articles.forEach((article) => {
+          expect(typeof article.article_id).toBe('number');
+          expect(typeof article.title).toBe('string');
+          expect(typeof article.topic).toBe('string');
+          expect(typeof article.author).toBe('string');
+          expect(typeof article.created_at).toBe('string');
+          expect(typeof article.votes).toBe('number');
+          expect(typeof article.article_img_url).toBe('string');
+          expect(typeof article.comment_count).toBe('number');
+        });
+      });
+  });
+});
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -2,7 +2,7 @@
 const {fetchArticles} = require('../models/index.models');
 
 const getArticles = (req, res) => {
-  console.log('hello from getArticles.controller');
+  // console.log('hello from getArticles.controller');
   fetchArticles().then((articles) => {
     console.log(articles);
     res.status(200).send({ articles });
