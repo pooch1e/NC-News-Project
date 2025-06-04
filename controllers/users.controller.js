6130e5473b6f2e99604f3fbc1757a11415ec9114
Joel Kram <joelnskram@gmail.com>
2025-06-04 16:12:17 +0100

    fix: cleaned comments and updated endpoints.json

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,7 @@
-const {fetchUser} = require('../models/index.models')
+const { fetchUser } = require('../models/index.models');
 const getUsers = (req, res) => {
-  console.log('hello from users controller')
   fetchUser().then((users) => {
-    
-    res.status(200).send({users});
-  })
-
-}
+    res.status(200).send({ users });
+  });
+};
 module.exports = getUsers;
