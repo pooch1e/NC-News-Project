947a70a630d8fe7468de6ac09306db78cdf4a4b5
Joel Kram <joelnskram@gmail.com>
2025-06-06 16:12:32 +0100

    added router.js file

--- app.js
+++ app.js
@@ -5,18 +5,9 @@ const app = express();
 // parse JSON
 app.use(express.json());
 
-// import controllers
-const {
-  getApi,
-  getTopics,
-  getArticles,
-  getArticleById,
-  getCommentsByArticleId,
-  postCommentByArticleId,
-  patchArticleById,
-  deleteCommentByCommentId,
-  getUsers,
-} = require('./controllers/index.controllers');
+// import Routers
+const apiRouter = require('./routes/apiRouter');
+app.use('/api', apiRouter);
 
 // import error handling
 const {
@@ -24,34 +15,6 @@ const {
   handleCustomError,
   handlePgErrors,
 } = require('./errors/index.errors');
-
-// --- HANDLE API ---
-// GET /api directory
-app.get('/api', getApi);
-
-// GET /topics
-app.get('/api/topics', getTopics);
-
-// GET /articles
-app.get('/api/articles', getArticles);
-
-// GET /api/articles/:article_id
-app.get('/api/articles/:article_id', getArticleById);
-
-// PATCH /api/articles/:article_id
-app.patch('/api/articles/:article_id', patchArticleById);
-
-// GET /api/articles/:article_id/comments
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
-
-// POST /api/articles/:article_id/comments
-app.post('/api/articles/:article_id/comments', postCommentByArticleId);
-
-// DELETE /api/comments/:comment_id
-app.delete('/api/comments/:comment_id', deleteCommentByCommentId);
-
-// GET /users
-app.get('/api/users', getUsers);
 
 // Catch 404 - invalid route
 app.use((req, res, next) => {
